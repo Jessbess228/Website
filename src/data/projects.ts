3bde9ed5615d52952c89bbf2640f5b149d93e6d7
JessBess228 <jessicaberrydev@gmail.com>
2026-09-24 15:41:27 +1000

    description/bio

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -28,7 +28,7 @@ export const projects: Project[] = [
       'A website prototype for a client, featuring a visual page builder to allow edits on their side.',
     stack: ['React', 'Vite', 'Puck'],
     body: [
-      'This is a Vite + React site that uses Puck as a visual layout manager and booking form which sends contents to the client email.',
+      'This is a Vite + React site that uses Puck as a visual layout manager. It includes a booking form to send bookings to the clients email.',
       'To edit copy and styling click the pen icon in the footer — On save, the page overwrites a JSON layout file, this is persisted in disk storage on my ec2 instance.',
     ],
   },
